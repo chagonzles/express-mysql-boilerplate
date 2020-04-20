a45b9c7c15ea34a26bb3d9cdbdc6cb650bb2863c
Cha Gonzales <charlene.gonzles@gmail.com>
2020-04-20 09:27:19 +0800

    Move model, controllers, routes, config folders under app folder

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
-const db = require('./models');
+const routes = require('./app/routes');
+const db = require('./app/models');
 
 const app = express();
 
